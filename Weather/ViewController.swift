c647d4f3e2cccd7b4bed4d54f4a409135a702b75
Yehor249 <egor.nedov11@gmail.com>
2024-06-29 23:07:58 +0300

    Добавил скопированый код из видео

---
+++ Weather/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Yegor Niedov on 26.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var weatherLable: UILabel!
+    @IBOutlet var getWeatherButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getWeatherButton.addTarget(self , action: #selector(didTapGetWaetherButton), for: .touchUpInside)
+    }
+
+    @objc func didTapGetWaetherButton() {
+        
+        // в ссылке сделлать интерполяцию
+        
+        let urlString = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true"
+        let url = URL(string: urlString)!
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data, let weather = try? JSONDecoder().decode(WeatherData.self, from: data) {
+                DispatchQueue.main.async {
+                    self.weatherLable.text = "\(weather.currentWeather.temperature)"
+                }
+            } else {
+                print("Fail")
+            }
+        }
+        task.resume()
+    }
+}
+
